3b5ed5465ba7dbf6c766123eb4d7fb7f8dabdf35
olga <oluciams84@gmail.com>
2021-11-14 16:04:43 -0500

    Se elimina div del componente Welcome

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-
-export const Welcome = (props)=>{
+const Welcome = (props)=>{
 
   return(
-    <div>
-      <h1>Hola {props.name}</h1>
-    </div>
+
+    <h1>Hola {props.name}</h1>
+    
   )
 }
+
+export default Welcome;
